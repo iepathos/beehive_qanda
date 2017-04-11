af16e4a4d884a39f36c840541b8f1922c5c97307
Glen Baker <iepathos@gmail.com>
2017-04-11 00:32:08 -0400

    initial commit qanda microservice

---
+++ qanda.go
@@ -0,0 +1,17 @@
+// Questions and Answers (audio responses)
+
+// qanda models
+type Question struct {
+	prompt string
+}
+
+type AudioArgument struct {
+	username string
+	audio    byte
+}
+
+// qanda views
+
+// get list of questions
+
+// post audio argument
